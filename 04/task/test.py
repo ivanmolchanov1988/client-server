5015a438fedb168979c0b84a5f00a6485b504293
Ivan <silencer1588@yandex.ru>
2021-05-13 15:08:56 +0300

    pre-commit lesson_4

---
+++ 04/task/test.py
@@ -0,0 +1,23 @@
+import unittest
+import server01
+import client01
+import argparse
+
+
+class Test_client_server(unittest.TestCase):
+    def test_server_parser(self):
+        test_parser = argparse.ArgumentParser()
+
+        self.assertEqual((server01.createParser().parse_args().addr,
+                          server01.createParser().parse_args().port),
+                         ('', 7777))
+
+    def test_client_parser(self):
+        self.assertEqual((client01.createParser().parse_args().addr,
+                          client01.createParser().parse_args().port),
+                         ('', 7777))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
