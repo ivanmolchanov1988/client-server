63dbf1ce2914ff021f0291f0d8b6372f8d218597
Ivan <silencer1588@yandex.ru>
2021-05-14 22:22:13 +0300

    Lesson_5 done

---
+++ 05/task/log/server_log_config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import logging
+from datetime import datetime
+
+logger = logging.getLogger('server_log')
+formatter = logging.Formatter('%(asctime)s    %(levelname)s ---> %(message)s   ')
+
+oTime = datetime.now().strftime('-%d-%m-%y')
+fh = logging.FileHandler(f"./log/server_log{oTime}.txt", encoding='utf-8')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
+
+### GO ###
+#logger.info('Тестовый запуск логирования')
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+console.setFormatter(formatter)
+logger.addHandler(console)
